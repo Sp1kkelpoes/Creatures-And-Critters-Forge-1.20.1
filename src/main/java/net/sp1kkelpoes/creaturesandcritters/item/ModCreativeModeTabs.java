9700d33c43c79068abc8f5adf2637966428a6653
Sp1kkelpoes <Lukaszvanhoogen@gmail.com>
2025-03-16 16:38:33 +0100

    Creative Tab Icon Fix

--- src/main/java/net/sp1kkelpoes/creaturesandcritters/item/ModCreativeModeTabs.java
+++ src/main/java/net/sp1kkelpoes/creaturesandcritters/item/ModCreativeModeTabs.java
@@ -19,7 +19,7 @@ public class ModCreativeModeTabs {
 
 
     public static final RegistryObject<CreativeModeTab> Creatures_And_Critters_Items_Tab = CREATIVE_MODE_TABS.register("creatures_and_critters_items_tab",
-            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.WEIRD_GEM.get()))
+            () -> CreativeModeTab.builder().icon(() -> new ItemStack(ModItems.GRASSLING_SPAWN_EGG.get()))
                     .title(Component.translatable("creativetab.creatures_and_critters_items_tab"))
                     .displayItems((itemDisplayParameters, output) -> {
                         output.accept(ModItems.GRASSLING_SPAWN_EGG.get());
